cc5a5fea220d6fa9642c657bba88de4c44d457f2
squeletteur <antoinemulliez@orange.fr>
2020-04-01 17:40:37 +0200

    début du fantôme

---
+++ Assets/Scripts/Fantome/GameManager_Fantome.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class MGhostObject
+{
+    public GameObject myObject;
+    public Sprite[] myPictures;
+    private bool isShowing = false;
+}
+
+public class GameManager_Fantome : MonoBehaviour
+{
+    public MGhostObject[] objectsToSpawnMGhost;
+    public SpriteRenderer[] emptys;
+
+    public float resetCD;
+    private float CDActual = 0;
+
+    private int score = 0;
+
+    public static GameManager_Fantome Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetEmptys();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CDActual += Time.deltaTime;
+
+        Debug.Log(score);
+
+        if (CDActual >= resetCD)
+        {
+            SetEmptys();
+        }
+    }
+
+    public void SetEmptys()
+    {
+        int objectToSpawn = Random.Range(0, objectsToSpawnMGhost.Length);
+
+        for (int i = 0; i < emptys.Length; i ++)
+        {
+            emptys[i].sprite = objectsToSpawnMGhost[objectToSpawn].myPictures[i];
+        }
+
+        CDActual = 0;
+    }
+
+    public void AddScore(int newScore)
+    {
+        score += newScore;
+
+    }
+}
---
+++ Assets/Scripts/Fantome/PhotoManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotoManager : MonoBehaviour
+{
+    public float maxDistance;
+
+    public GameObject test;
+
+    public Camera photo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MakePhoto();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, test.transform.position);
+    }
+
+    public void MakePhoto()
+    {
+        GameObject[] ghosts = GameObject.FindGameObjectsWithTag("Ghost");
+
+        foreach (GameObject myGhosts in ghosts)
+        {
+            test = myGhosts;
+            RaycastHit hit;
+
+            var direction = myGhosts.transform.position - transform.position;
+
+            if (Physics.Raycast(transform.position, direction, out hit, maxDistance))
+            {
+                if (hit.transform.gameObject.CompareTag("Ghost"))
+                {
+                    Vector3 screenPoint = Camera.main.WorldToViewportPoint(myGhosts.transform.position);
+                    bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+
+                    if (onScreen)
+                    {
+                        int addScore = hit.transform.gameObject.GetComponent<FantomeScript>().GetMyScore();
+                        GameManager_Fantome.Instance.AddScore(addScore);
+                    }
+                }
+            }
+        }
+    }
+}
