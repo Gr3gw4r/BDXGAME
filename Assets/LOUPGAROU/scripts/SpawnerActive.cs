5962d108309629035745f117fca8acb483680096
phur0ncle <gregoire.routier@outlook.fr>
2020-04-10 10:06:09 +0200

    spawner avancement

---
+++ Assets/LOUPGAROU/scripts/SpawnerActive.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerActive : MonoBehaviour
+{
+    public GameObject spawner;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("WaveSpawn"))
+        {
+            GameObject == true; 
+        }
+        else
+        {
+
+        }
+    }
+}
