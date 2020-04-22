a478e3ccc9afffecff7928ab1d422c9590c39fa4
phur0ncle <gregoire.routier@outlook.fr>
2020-04-22 10:08:56 +0200

    Heal

---
+++ Assets/LOUPGAROU/scripts/Heal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heal : MonoBehaviour
+{
+    public GameObject jambon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Heal"))
+        {
+            Debug.Log("Soin");
+        }
+        Destroy(jambon);
+    }
+}
