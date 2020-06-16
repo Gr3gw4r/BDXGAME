4a4dbfad25de557f70f6dbe27b03b90d68e0d50c
squeletteur <antoinemulliez@orange.fr>
2020-06-16 15:11:15 +0200

    loup-garou

--- Assets/LOUPGAROU/scripts/Tue.cs
+++ Assets/LOUPGAROU/scripts/Tue.cs
@@ -6,11 +6,13 @@ public class Tue : MonoBehaviour
 {
     public float Damage;
 
+    public GameObject bloodParticles;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Enemy"))
         {
-            Debug.Log("salut");
+            Instantiate(bloodParticles, transform.position, Quaternion.identity);
             other.GetComponent<Enemy>().Death();
         }
 
